f5cfeb492f1d301fce70fc060e8e7d789bea674a
Eric Chen <ec1348@gmail.com>
2023-04-02 19:05:28 +0800

    create member web server routes

--- member/app/routes/member.routes.js
+++ member/app/routes/member.routes.js
@@ -2,5 +2,5 @@ module.exports = app => {
   const router = require('express').Router()
   const memberController = require("../controllers/member.controller");
   app.use("/", router)
-  router.get("/members", memberController.queryMembers);
+  router.post("/signup", memberController.memberSignUp)
 }
