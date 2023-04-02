443378b53d264ff2ccfd2a863ac6e46e8b3be3b1
Eric Chen <ec1348@gmail.com>
2023-04-02 19:04:31 +0800

    Create member express web server

--- member/index.js
+++ member/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 // 設定swagger
 const swaggerUI = require('swagger-ui-express')
 const swaggerConfig = require("./app/config/swagger.config");
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerConfig.createSwaggerDocs()))
 
+global.env = 'dev'
 app.get('/', function (req, res) {
     res.send('Welcome to the backend service of member api management ! ! !')
 })
