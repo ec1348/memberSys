99e8020dd97463302392c52102c5fa22500ce949
Eric Chen <ec1348@gmail.com>
2023-04-02 19:05:01 +0800

    create member web server controllers

--- member/app/controllers/member.controller.js
+++ member/app/controllers/member.controller.js
@@ -1,9 +1,60 @@
-// Get all the members
-exports.queryMembers = (req, res) => {
-    res.send([{
-        id: 1, 
-        firstName: 'Eric',
-        LastName: 'Chen',
-        age: 27
-    }])
+const db = require('../models')
+const bcrypt = require('bcrypt')
+
+let hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+  return hashedPassword
+}
+
+exports.memberSignUp = async (req, res) => {
+  let {
+    userName,
+    password,
+    email,
+    firstName,
+    lastName
+  } = req.body
+  let hashedPassword = await hashPassword(password)
+
+  // check userName
+  await db.Member.findOne({where: {userName: userName}})
+    .then(member => {
+      if(member){
+        res.send({
+          message: 'userName has been used'
+        })
+      }
+    })
+    .catch(err => {
+      console.log('err')
+    })
+
+  // check email
+  await db.Member.findOne({where: {email: email}})
+    .then(member => {
+      if(member){
+        res.send({
+          message: 'email has been used'
+        })
+      }
+    })
+  
+  
+  db.Member.create({
+    userName: userName,
+    password: hashedPassword,
+    email: email,
+    firstName: firstName,
+    lastName: lastName
+  })
+    .then(member => {
+      res.send({
+        message: 'add new member successfully',
+        newMember: member
+      })
+  })
+    .catch(err => {
+      res.err(err)
+  })
 };
