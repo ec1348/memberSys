4d4e76f063e9ed84c0d5d4d01d1f359f9d2b2c27
Eric Chen <ec1348@gmail.com>
2023-04-09 19:41:54 +0800

    Modify db config file

--- member/app/config/db.config.js
+++ member/app/config/db.config.js
@@ -1,13 +1,62 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "member",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+createDBconfig = () => {
+  if (process.env.NODE_ENV === 'test') {
+    return({
+      HOST: "localhost",
+      USER: "root",
+      PASSWORD: "123456",
+      DB: "memberTest",
+      dialect: "mysql",
+      port: 3307,
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    })
+  } else if(process.env.NODE_ENV === 'dev') {
+    return({
+      HOST: "localhost",
+      USER: "root",
+      PASSWORD: "123456",
+      DB: "memberDev",
+      dialect: "mysql",
+      port: 3306,
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    })
+  } else if(process.env.NODE_ENV === 'qa') {
+    return({
+      HOST: "localhost",
+      USER: "root",
+      PASSWORD: "123456",
+      DB: "member",
+      dialect: "mysql",
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    })
+  } else {
+    return({
+      HOST: "localhost",
+      USER: "root",
+      PASSWORD: "123456",
+      DB: "member",
+      dialect: "mysql",
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    })
   }
 }
+module.exports = createDBconfig()
