42bc93b9178b63271781acf4b66aac252aa524c1
Eric Chen <ec1348@gmail.com>
2023-05-08 17:44:09 +0800

    Add condition to deploy/1.dockerBuildEc2.sh file

--- devops/build/1.dockerBuildEc2.sh
+++ devops/build/1.dockerBuildEc2.sh
@@ -1,2 +1,4 @@
+# docker build
 docker build -t member:latest .
-docker rmi $(docker images -f dangling=true -q)
+# remove dangling image
+if [ $(docker images -f dangling=true -q | wc -l) -gt 1 ]; then docker rmi $(docker images -f dangling=true -q); fi
