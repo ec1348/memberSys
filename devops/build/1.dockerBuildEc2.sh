18d75b5c95c27632f33762fc19292f2bd8bd8440
Eric Chen <ec1348@gmail.com>
2023-05-09 11:02:18 +0800

    Fix: Add force tag to docker rmi

--- devops/build/1.dockerBuildEc2.sh
+++ devops/build/1.dockerBuildEc2.sh
@@ -1,4 +1,4 @@
 # docker build
 docker build -t member:latest .
 # remove dangling image
-if [ $(docker images -f dangling=true -q | wc -l) -gt 1 ]; then docker rmi $(docker images -f dangling=true -q); fi
+if [ $(docker images -f dangling=true -q | wc -l) -gt 1 ]; then docker rmi -f $(docker images -f dangling=true -q); fi
